48636a84aff1ebe43c5473abc7ea1c91f215f444
ximu-tao <me_litao@outlook.com>
2022-11-15 12:33:21 +0800

    ShowDOS 解耦, 增强复用性 可以在任意输出流打印游戏界面

--- src/com/outlook/me_litao/view/ShowDOS.java
+++ src/com/outlook/me_litao/view/ShowDOS.java
@@ -1,8 +1,17 @@
 package com.outlook.me_litao.view;
 
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class ShowDOS extends Show {
+    PrintStream out;
+    public ShowDOS(PrintStream printStream){
+        out = printStream;
+    }
+    public ShowDOS(){
+        out = System.out;
+    }
+
     
     String gap = " ";
 
@@ -13,12 +22,12 @@ public class ShowDOS extends Show {
     }
 
     private void welcome(){
-        System.out.println("欢迎来到推箱子小游戏");
+        out.println("欢迎来到推箱子小游戏");
     }
 
     private void showTip(){
-        System.out.println("游戏目标: 将箱子(口)推到目标点(丶), 控制你的人物(人)开始行动吧! ");
-        System.out.println("提示: (回)表示 箱子在目标点上, (太)表示 人物在目标点上");
+        out.println("游戏目标: 将箱子(口)推到目标点(丶), 控制你的人物(人)开始行动吧! ");
+        out.println("提示: (回)表示 箱子在目标点上, (太)表示 人物在目标点上");
     }
     
     
@@ -58,39 +67,39 @@ public class ShowDOS extends Show {
     
     public void showNULL(){
         // 半角空格地图会乱, 所以这里使用全角空格
-        System.out.print("　"+gap);
+        out.print("　"+gap);
     }
     
     public void showTarger(){
-        System.out.print("丶"+gap);
+        out.print("丶"+gap);
     }
     public void showWall(){
-        System.out.print("墙"+gap);
+        out.print("墙"+gap);
         
     }
     public void showBox(){
-        System.out.print("口"+gap);
+        out.print("口"+gap);
         
     }
     public void showCharacter(){
-        System.out.print("人"+gap);
+        out.print("人"+gap);
 
     }
 
     public void showCharacterAndTarger(){
-        System.out.print("太"+gap);
+        out.print("太"+gap);
     }
 
     public void showBoxAndTarger(){
-        System.out.print("回"+gap);
+        out.print("回"+gap);
     }
 
     public void newLine(){
-        System.out.print("\n");
+        out.print("\n");
     }
 
     public void winning(){
-        System.out.print("\n" +
+        out.print("\n" +
                 "\n" +
                 "           _             _             \n" +
                 "          (_)           (_)            \n" +
